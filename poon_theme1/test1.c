4f61cbb66840bf827c0933466e73794f90108f3c
napatlimsuwan <167305685+napatlimsuwan@users.noreply.github.com>
2024-12-10 16:03:48 +0900

    Update test1.c

--- poon_theme1/test1.c
+++ poon_theme1/test1.c
@@ -6,7 +6,7 @@ int main() {
     printf("Napat limsuwan\n");    // Check if the program is running
     while(1) {
         scanf("%c", &buffer);      // Read a character from the keyboard
-        if (bufffer != 'q') {
+        if (buffer != 'q') {
             printf("%c", buffer);  // Print the character
         } else {
             break;                 // Exit the loop if the character is 'q'
